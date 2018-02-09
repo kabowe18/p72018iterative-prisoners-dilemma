49b252ef59411e2ab330f4a21d7077d3beba2208
drmay18 <drmay18@students.shrewsbury.k12.ma.us>
2018-02-09 13:21:45 -0500

    the name, strategy name and description were changed

--- team6.py
+++ team6.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Gucci Gang' # Only 10 chars displayed.
+strategy_name = 'Winning'
+strategy_description = 'If there score is greater than zero, we betray. If our score is  '
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
